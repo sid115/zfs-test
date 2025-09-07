873a6cf4f1a9beee1f284ba25fe89ed0dabb868a
sid <sid@portuus.de>
2025-09-07 14:47:27 +0200

    automount dpool

--- hosts/zfs-test/zfs.nix
+++ hosts/zfs-test/zfs.nix
@@ -7,7 +7,7 @@
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.grub.enable = true;
   boot.loader.grub.zfsSupport = true;
-  # boot.zfs.extraPools = [ "dpool" ];
+  boot.zfs.extraPools = [ "dpool" ];
 
   services.openssh.settings.PermitRootLogin = "prohibit-password";
   users.users.root.openssh.authorizedKeys.keyFiles = [ ../../users/root/pubkeys/zfs-replication.pub ];
