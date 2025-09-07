1fd1ac762327f2d0c4bfb2dfb154380424a92ce1
sid <sid@portuus.de>
2025-09-07 14:19:26 +0200

    enabled zfs

--- apps/rebuild/rebuild.sh
+++ apps/rebuild/rebuild.sh
@@ -1,6 +1,7 @@
 USER="$(whoami)"
-HOST="portuus"
-DOMAIN="portuus.de"
+HOST="zfs-test"
+TARGET_HOST="91.98.86.229"
+BUILD_HOST="portuus.de"
 PORT="2299"
 
-NIX_SSHOPTS="-p $PORT" rebuild nixos -p . -H "$HOST" -B "$USER"@"$DOMAIN" -T "$USER"@"$DOMAIN"
+NIX_SSHOPTS="-p $PORT" rebuild nixos -p . -H "$HOST" -B "$USER"@"$BUILD_HOST" -T "$USER"@"$TARGET_HOST"
--- hosts/zfs-test/zfs.nix
+++ hosts/zfs-test/zfs.nix
@@ -1,4 +1,14 @@
+{ lib, ... }:
+
 {
+  networking.hostId = "f4a3bcf1"; # head -c 8 /etc/machine-id
+
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.zfsSupport = true;
+  # boot.zfs.extraPools = [ "dpool" ];
+
   services.openssh.settings.PermitRootLogin = "prohibit-password";
   users.users.root.openssh.authorizedKeys.keyFiles = [ ../../users/root/pubkeys/zfs-replication.pub ];
 }
